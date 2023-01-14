03b4f841268a01649b1675370a1baedc41317b14
Tausif4171 <tausifkhan4171@gmail.com>
2023-01-14 18:42:42 +0530

    structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { QuestionCard } from "./components/QuestionCard";
 
 function App() {
-  return <div className="App">Quiz App</div>;
+  const startTrivia = async () => {};
+  const checkAnswer = () => {};
+  const nextQuestion = () => {};
+  return (
+    <div className="App">
+      <h3>Quiz App</h3>
+      <button onClick={startTrivia}>Start</button>
+      <p>Score:</p>
+      <QuestionCard />
+      <button onClick={nextQuestion}>Next Question</button>
+    </div>
+  );
 }
 
 export default App;
