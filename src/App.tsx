64c492c09bfefb62554190c0db6ddea810ea681d
Tausif4171 <tausifkhan4171@gmail.com>
2023-01-18 22:43:34 +0530

    planned states for quiz app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { QuestionCard } from "./components/QuestionCard";
-
+const TOTAL_QUESTIONS = 10;
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
   const startTrivia = async () => {};
   const checkAnswer = () => {};
   const nextQuestion = () => {};
